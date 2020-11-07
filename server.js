baf0ee59d16f80db9159fc16ded59ea52b0a1892
Felix <felixwochnick@gmail.com>
2020-11-07 14:04:18 +0100

    working

--- server.js
+++ server.js
@@ -24,9 +24,11 @@ app.set('view engine', 'ejs');
 app.get('/', (req, res) => res.render('index.ejs', {version: require('./package.json').version}));
 
 app.get('/shared', (req, res) => {
-    let id = req.query('id');
+    let id = req.query.id;
     let db = new sqlite.Database('./data/data.db');
     db.run(`SELECT val FROM shared_notes WHERE id=${id};`, (err) => {
+        if (err) {console.log(err); return;}
+
         console.log(this);
 
         let note = {};
@@ -41,10 +43,11 @@ app.get('/shared', (req, res) => {
 app.post('/share', (req, res) => {
     let note = req.body.note;
     let uuid = uuidv4();
+    let expiry_date = ''; // TODO: generate expiry date
     let db = new sqlite.Database('./data/data.db', sqlite.OPEN_READWRITE);
 
     db.serialize(() => {
-        db.run(`INSERT INTO shared_notes (id, val) VALUES (${uuid}, ${note})`, (err) => {
+        db.run(`INSERT INTO shared_notes (id, val, expiry_date) VALUES (${uuid}, ${note}, ${expiry_date})`, (err) => {
             if(err){
                 res.json({status: 'failed'})
                 db.close();

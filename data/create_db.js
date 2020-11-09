584b857c3f98c9faa146d7589cb6e40ea19f302b
Felix <felixwochnick@gmail.com>
2020-11-09 16:50:59 +0100

    #4: basic functionality added

--- data/create_db.js
+++ data/create_db.js
@@ -3,8 +3,32 @@ const sqlite3 = require('sqlite3').verbose();
 
 let db = new sqlite3.Database('./data.db');
 
-db.serialize(() => {
-    db.run(`CREATE TABLE shared_notes (id INTEGER PRIMARY KEY, val TEXT DEFAULT '', expiry_date TEXT NOT NULL);`, (err) => console.log((err) ? err : 'Success'))
+// db.serialize(() => {
+//     db.run(`CREATE TABLE shared_notes (id TEXT PRIMARY KEY, val TEXT DEFAULT '', expiry_date TEXT NOT NULL);`, (err) => console.log((err) ? err : 'Success'))
+// });
+
+// db.run(`DELETE FROM shared_notes`, (err) => {
+//     if(err){
+//         console.log(err);
+        
+//     }
+//     else{
+//         console.log('Success');
+//     }
+// });
+
+// db.run(`INSERT INTO shared_notes (id, val, expiry_date) VALUES ('2', '{}', '07-11-2020')`, (err) => {
+//     if(err){
+//         console.log(err);
+        
+//     }
+//     else{
+//         console.log('Success');
+//     }
+// });
+
+db.all('SELECT * FROM shared_notes', (err, rows) => {
+    console.log(rows);
 });
 
 db.close();
--- server.js
+++ server.js
@@ -23,43 +23,47 @@ app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('index.ejs', {version: require('./package.json').version}));
 
-app.get('/shared', (req, res) => {
-    let id = req.query.id;
-    let db = new sqlite.Database('./data/data.db');
-    db.run(`SELECT val FROM shared_notes WHERE id=${id};`, (err) => {
-        if (err) {console.log(err); return;}
-
-        console.log(this);
-
-        let note = {};
-        res.render('shared.ejs', {version: require('./package.json').version, note});
-    });
-});
+app.get('/shared', (req, res) => res.render('shared.ejs', {version: require('./package.json').version}));
 
 // +--------+
 // |  POST  |
 // +--------+
 
-app.post('/share', (req, res) => {
+app.post('/api/note/share', (req, res) => {
     let note = req.body.note;
     let uuid = uuidv4();
-    let expiry_date = ''; // TODO: generate expiry date
+    let expiry_date = new Date();
+    expiry_date.setDate(expiry_date.getDate() + 1);
+    expiry_date = `${expiry_date.getDate()}-${expiry_date.getMonth() + 7}-${expiry_date.getFullYear()}`;
     let db = new sqlite.Database('./data/data.db', sqlite.OPEN_READWRITE);
 
     db.serialize(() => {
-        db.run(`INSERT INTO shared_notes (id, val, expiry_date) VALUES (${uuid}, ${note}, ${expiry_date})`, (err) => {
+        db.run(`INSERT INTO "shared_notes" ("id", "val", "expiry_date") VALUES (?, ?, ?);`, [uuid.toString(), JSON.stringify(note), expiry_date], (err) => {
             if(err){
                 res.json({status: 'failed'})
-                db.close();
-                return;
             }
+            else{
+                res.json({status: 'success', id: uuid});
+            }
+        });
+    });
 
-            res.json({status: 'success', id: uuid})
+    db.close();
+});
 
-            db.close();
-        })
-    })
+app.post('/api/note/get', (req, res) => {
+    let id = req.body.id;
 
+    let db = new sqlite.Database('./data/data.db');
+
+    db.get(`SELECT * FROM shared_notes WHERE id=?;`, [id], (err, row) => {
+        if (err) {console.log(err); res.json({status: "failed"}); return;}
+        let note = JSON.parse(row.val);
+        note.id = 0;
+        res.json({status: "success", note});
+    });
+
+    db.close();
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
